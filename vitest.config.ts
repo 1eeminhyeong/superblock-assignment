5661d632ebfdd72d15896e9f033b811e529c56b7
이민형 <peterlee11@naver.com>
2024-12-19 14:13:57 +0900

    Add jsdom for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     name: packageJson.name,
+    passWithNoTests: true,
     dir: "./src",
     environment: "jsdom",
     setupFiles: "./vitest.setup.ts",
